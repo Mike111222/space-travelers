50db8c718e6c2ed913215a628f5e863585a56e78
Mike111222 <Tipu870@gmail.com>
2023-07-25 20:57:17 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import './styles/rockets.css';
 import Navbar from './components/Navbar';
 import Missions from './components/Missions';
-// import MyProfile from './components/MyProfile';
 
 function App() {
   return (
@@ -13,8 +11,7 @@ function App() {
         <Navbar />
         <Routes>
           {/* <Route exact path="/" element={<MyProfile />} /> */}
-          <Route exact path="missions" element={<Missions />} />
-
+          <Route path="/missions" element={<Missions />} />
         </Routes>
       </div>
     </>
